a472108d8c0ee2f926eaa2ff16a667bce812d972
magralol <gnorr_92@hotmail.com>
2015-02-06 11:48:53 +0100

    fixed git conflicts

--- laravel/app/controllers/HomeController.php
+++ laravel/app/controllers/HomeController.php
@@ -2,28 +2,8 @@
 
 class HomeController extends BaseController {
 
-<<<<<<< HEAD
-	/*
-	|--------------------------------------------------------------------------
-	| Default Home Controller
-	|--------------------------------------------------------------------------
-	|
-	| You may wish to use controllers instead of, or in addition to, Closure
-	| based routes. That's great! Here is an example controller method to
-	| get you started. To route to this controller, just add the route:
-	|
-	|	Route::get('/', 'HomeController@showWelcome');
-	|
-	*/
-
-	public function showWelcome()
-	{
-		//return View::make('hello');
-        return "hello world! :o";
-=======
 	public function index(){
 		return View::make('home');
->>>>>>> master
 	}
 
 }
--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -1,9 +1,4 @@
 <?php
 
-Route::get("/", "HomeController@showWelcome");
-
-<<<<<<< HEAD
-Route::get("/search/{key}/{val}", "SearchController@index");
-=======
 Route::get('/', 'HomeController@index');
->>>>>>> master
+Route::get("/search/{key}/{val}", "SearchController@index");
