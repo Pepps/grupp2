693548fa7c6f5ccb7f27257b5de4b8250dd6bfe0
Jesper <jesper.hellberg@live.com>
2015-02-12 15:20:09 +0100

    Jesper Commit Slutet av dagen till CM

--- laravel/app/routes.php
+++ laravel/app/routes.php
@@ -29,49 +29,6 @@ Route::group(array('prefix' => 'api/v1', 'before' => 'auth.basic'), function()
     Route::resource('url', 'UrlController');
 });
 
-// route to process the ducks form
-Route::post('/ducks', function()
-{
-
-    // process the form here
-
-    // Skapar validerande regler ----------
-    $rules = array(
-        'name'      => 'required',
-    // bara en klassisk validering för ditt namn!
-        'email'     => 'required|email|unique:ducks',
-    // Ett krav samt ett unikt värde i "duck" tabellen
-        'password'  => 'required',
-        'password_confirm'  => 'required|same:password',
-    // kräver lösenord som även måste matcha fälten
-    );
-
-    $validator = Validator::make(Input::all(), $rules);
-
-    if ($validator->fails()) {
-
-        // Felmeddelanden från validatorn
-        $messages = $validator->messages();
-
-        return Redirect::to('/ducks')
-            -> withErrors($validator)
-            -> withInput(Input::except('password', 'password_confirm'));
-
-    } else {
-
-        $duck = new Duck;
-        $duck->name         = Input::get('name');
-        $duck->email        = Input::get('email');
-        $duck->password     = Hash::make(Input::get('password'));
-
-        //spara våran anka
-        $duck->save();
-
-        //redirect -------------
-        return Redirect::to('ducks');
-    }
-
-});
-
+Route::resource('experiences', 'ExperienceController');
 Route::get('/', 'HomeController@index');
 Route::get("/search/{key}/{val}", "SearchController@index");
